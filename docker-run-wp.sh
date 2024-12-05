3a465f2cb7cbb17ee8a0f736afe11a7457462ea9
jcdacula7 <jcdacula7@gmail.com>
2024-12-06 05:29:56 +0800

    modify .gitignore, docker-run-wp.sh, add index.php on app, plugins and themes folder

--- docker-run-wp.sh
+++ docker-run-wp.sh
@@ -1,17 +1,19 @@
-docker build -t . wplocal:latest
+docker build -t wplocal:latest .
 
 docker run -d \
   -p 8886:80 \
-  --name wplocal \
-  --network netfone-network \
+  --name wp-container \
+  --network wplocal-network \
   -v ./web/app/plugins:/var/www/html/wp-content/plugins \
   -v ./web/app/themes:/var/www/html/wp-content/themes \
   -e WORDPRESS_DB_PASSWORD=dbpassword \
-  -e WORDPRESS_ADMIN_PASSWORD=adminroot \
+  -e WORDPRESS_ADMIN_PASSWORD=adminroot \ 
   wplocal:latest
+
 
 docker run -d \
   --name mysql-container \
+  --network wplocal-network \
   -e MYSQL_ROOT_PASSWORD=yourpassword \
   -p 3307:3306 \
   mysql:latest
